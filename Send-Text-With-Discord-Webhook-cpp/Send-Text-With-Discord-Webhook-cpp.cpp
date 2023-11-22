7921efe89e722caeda5e96da0b912d7f1a0f6ff8
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-11-22 13:19:18 +0600

    Add project files.

---
+++ Send-Text-With-Discord-Webhook-cpp/Send-Text-With-Discord-Webhook-cpp.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <windows.h>
+
+int system_no_output(std::string command)
+{
+    command.insert(0, "/C ");
+
+    SHELLEXECUTEINFOA ShExecInfo = { 0 };
+    ShExecInfo.cbSize = sizeof(SHELLEXECUTEINFO);
+    ShExecInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
+    ShExecInfo.hwnd = NULL;
+    ShExecInfo.lpVerb = NULL;
+    ShExecInfo.lpFile = "cmd.exe";
+    ShExecInfo.lpParameters = command.c_str();
+    ShExecInfo.lpDirectory = NULL;
+    ShExecInfo.nShow = SW_HIDE;
+    ShExecInfo.hInstApp = NULL;
+
+    if (ShellExecuteExA(&ShExecInfo) == FALSE)
+        return -1;
+
+    WaitForSingleObject(ShExecInfo.hProcess, INFINITE);
+
+    DWORD rv;
+    GetExitCodeProcess(ShExecInfo.hProcess, &rv);
+    CloseHandle(ShExecInfo.hProcess);
+
+    return rv;
+}
+-
+int main()
+{
+    std::string url = "https://discord.com/api/webhooks/1176782836141928521/pKJbmJJp_v8IYCKdA0qM5VFRFWlQWOWhpsDVu5jsyyQ47oGbFQg4Pz7FCrZRyLsgrl_x";
+    const std::string cmd_1 = "curl -i -H \"Accept: application/json\" -H \"Content-Type:application/json\" -X POST --data \"{\\\"content\\\": \\\"";
+    const std::string cmd_2 = "\\\"}\" ";
+    std::string command = cmd_1 + "Hello From C++ Console Application without console output" + cmd_2 + url;
+    system_no_output(command.c_str());
+}
